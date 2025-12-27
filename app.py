91d8ed1939556800171cb91bd4c4028cd660e8df
selvidakshna <selvimalini83@gmail.com>
2025-12-27 20:02:44 +0530

    Deploy on HuggingFace

--- app.py
+++ app.py
@@ -81,6 +81,6 @@ def grammar_predict():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", port=port)
 
